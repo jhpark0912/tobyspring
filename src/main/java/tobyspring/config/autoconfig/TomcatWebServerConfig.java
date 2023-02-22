6fc2b84d2f675249786a2cb6cc451358496c2c6b
jhpark0912 <jhprk0912@fasoo.com>
2023-02-22 09:38:57 +0900

    02/22 Conditional 세팅과 라이브러리에 따른 사용 선택 까지

--- src/main/java/tobyspring/config/autoconfig/TomcatWebServerConfig.java
+++ src/main/java/tobyspring/config/autoconfig/TomcatWebServerConfig.java
@@ -2,15 +2,25 @@ package tobyspring.config.autoconfig;
 
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.*;
+import org.springframework.core.type.AnnotatedTypeMetadata;
+import org.springframework.util.ClassUtils;
 import tobyspring.config.MyAutoConfiguration;
 
 @MyAutoConfiguration
+@Conditional(TomcatWebServerConfig.TomcatCondition.class)
 public class TomcatWebServerConfig {
 
-    @Bean
+    @Bean("tomcatWebServerFactory")
     public ServletWebServerFactory servletWebServerFactory() {
         return new TomcatServletWebServerFactory();
+    }
+
+    static class TomcatCondition implements Condition {
+        @Override
+        public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
+            return ClassUtils.isPresent("org.apache.catalina.startup.Tomcat",
+                    context.getClassLoader());
+        }
     }
 }
